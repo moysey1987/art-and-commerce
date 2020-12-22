2fd35fc9447e68340798b005c1bfd58908ab87d7
moysey1987 <nigelcmoyes@gmail.com>
2020-12-22 22:41:08 +1100

    Add smooth scrolling

---
+++ main.js
@@ -0,0 +1,13 @@
+const anchors = document.querySelectorAll("header nav a")
+
+anchors.forEach(anchor => {
+    anchor.addEventListener('click', event => {
+        const href = anchor.getAttribute('href')
+        if (href.charAt(0) === '#') {
+            event.preventDefault()
+            document.querySelector(href).scrollIntoView({
+                behavior: 'smooth'
+            })
+        }
+    })
+})
