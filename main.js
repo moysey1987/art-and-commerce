5864ba9df31f68658457d6c718f15f96f7d90b96
moysey1987 <nigelcmoyes@gmail.com>
2020-12-23 17:57:42 +1100

    Finish services section

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ const anchors = document.querySelectorAll("header nav a")
 
 const parallaxDividers = document.querySelectorAll("div.image-divider-container")
 
-
+const serviceOneTag = document.querySelector("div.service-one")
+const serviceOneDescription = document.querySelector("div.s1-description")
+const serviceTwoTag = document.querySelector("div.service-two")
+const serviceTwoDescription = document.querySelector("div.s2-description")
+const serviceThreeTag = document.querySelector("div.service-three")
+const serviceThreeDescription = document.querySelector("div.s3-description")
+const serviceFourTag = document.querySelector("div.service-four")
+const serviceFourDescription = document.querySelector("div.s4-description")
 
 
 
@@ -33,4 +40,35 @@ parallaxDividers.forEach(divider => {
         divider.style.backgroundPositionY = offset * 0.3 + "px"
     })
 
+})
+
+// SERVICE DESCRIPTION REVEAL
+
+serviceOneTag.addEventListener("mouseover", function() {
+    serviceOneDescription.style.opacity = "1"
+})
+serviceOneTag.addEventListener("mouseleave", function() {
+    serviceOneDescription.style.opacity = "0"
+})
+
+
+serviceTwoTag.addEventListener("mouseover", function() {
+    serviceTwoDescription.style.opacity = "1"
+})
+serviceTwoTag.addEventListener("mouseleave", function() {
+    serviceTwoDescription.style.opacity = "0"
+})
+
+serviceThreeTag.addEventListener("mouseover", function() {
+    serviceThreeDescription.style.opacity = "1"
+})
+serviceThreeTag.addEventListener("mouseleave", function() {
+    serviceThreeDescription.style.opacity = "0"
+})
+
+serviceFourTag.addEventListener("mouseover", function() {
+    serviceFourDescription.style.opacity = "1"
+})
+serviceFourTag.addEventListener("mouseleave", function() {
+    serviceFourDescription.style.opacity = "0"
 })
